66a11c3c47e6fa622b9c8788194ea3aeea150e02
Anik Das <anikdas1997@outlook.com>
2025-01-27 21:30:25 +0530

    Print Reversed Table

---
+++ nTableInReverseOrder.py
@@ -0,0 +1,10 @@
+## function to print the table in reverse order
+def ReverseTable(n: int):
+    i = 10
+    while (i > 0):
+        print(i*n, sep=" ")
+        i -= 1
+
+## input
+n = 2
+ReverseTable(n)
